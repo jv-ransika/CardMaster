06fb88f7d4b55d9f20945b0ccbb398d100b96430
tharushaudana <tharushaudana519@gmail.com>
2025-08-19 23:41:34 +0530

    feat: implement device address management with shared preferences in BotScreen

--- lib/screens/bot/bot.dart
+++ lib/screens/bot/bot.dart
@@ -12,12 +12,14 @@ import 'package:card_master/screens/bot/connections_view.dart';
 import 'package:card_master/screens/bot/game_view.dart';
 import 'package:card_master/screens/bot/logs_view.dart';
 import 'package:card_master/screens/bot/test_view.dart';
+import 'package:card_master/screens/qr_scan/qr_scan.dart';
 import 'package:card_master/tflite/tflite_model_isolate.dart';
 import 'package:card_master/tflite/yolo_detector.dart';
 import 'package:image/image.dart' as img;
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:permission_handler/permission_handler.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 enum DeviceStatus { disconnected, connecting, connected }
 
@@ -41,10 +43,13 @@ class _BotScreenState extends State<BotScreen> {
   List<Widget> tabs = [];
 
   bool isLoadingModels = true;
+  bool isLoadingAddresses = true;
 
   TfliteModelIsolate yoloModel = TfliteModelIsolate(modelPath: "assets/yolo11s_f32.tflite");
   // TfliteModelIsolate yoloModel = TfliteModelIsolate(modelPath: "assets/yolov5nu_f16.tflite");
   OnnxModel oomiModel = OnnxModel(modelPath: "assets/oomi_agent.onnx");
+
+  List<String>? deviceAddresses = [];
 
   YoloDetector yoloDetector = YoloDetector();
   OomiPredictor oomiPredictor = OomiPredictor();
@@ -137,7 +142,26 @@ class _BotScreenState extends State<BotScreen> {
     }
   }
 
-  Future<void> detectCurrentImage() async {
+  Future<void> _loadDeviceAddresses() async {
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+    deviceAddresses = prefs.getStringList('addresses');
+    isLoadingAddresses = false;
+    setState(() {});
+  }
+
+  Future<void> _clearCurrentDeviceAddresses() async {
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+    await prefs.remove('addresses');
+    deviceAddresses = [];
+  }
+
+  Future<void> _saveDeviceAddresses() async {
+    if (deviceAddresses == null) return;
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+    await prefs.setStringList('addresses', deviceAddresses!);
+  }
+
+  Future<void> _detectCurrentImage() async {
     try {
       await yoloDetector.detectObjects(_currentImage!, yoloModel);
       ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text("YOLO detection completed.")));
@@ -200,6 +224,7 @@ class _BotScreenState extends State<BotScreen> {
     _initializeTabs();
 
     WidgetsBinding.instance.addPostFrameCallback((_) {
+      _loadDeviceAddresses();
       _loadModels();
     });
 
@@ -211,7 +236,7 @@ class _BotScreenState extends State<BotScreen> {
         updateGameView();
       },
       onSayTrumpSuit: () {
-        debugPrint("Trump Suit: ${gameHandler.trumpSuit}");
+        debugPrint("Trump Suittt: ${gameHandler.trumpSuit}");
         _updateRemote("Trump Suit Updated");
         updateGameView();
       },
@@ -291,7 +316,7 @@ class _BotScreenState extends State<BotScreen> {
       _currentImage = imageInputHandlerOuter.image!;
       // Rotate 180 degrees
       _currentImage = img.copyRotate(_currentImage!, angle: 180);
-      await detectCurrentImage();
+      await _detectCurrentImage();
       gameHandler.analyzeOuterCamDetections(yoloDetector.detections!);
       //...
       camerasViewController.outerImage = yoloDetector.processedImage;
@@ -308,7 +333,7 @@ class _BotScreenState extends State<BotScreen> {
       final originalImage = imageInputHandlerInner.image!;
       _currentImage = img.copyExpandCanvas(originalImage, newWidth: 1200, newHeight: 1200, backgroundColor: img.ColorRgb8(255, 255, 255));
       //============================================================================
-      await detectCurrentImage();
+      await _detectCurrentImage();
       gameHandler.analyzeInnerCamDetections(yoloDetector.detections!);
       //...
       camerasViewController.innerImage = yoloDetector.processedImage;
@@ -416,12 +441,20 @@ class _BotScreenState extends State<BotScreen> {
           RemotePlayStatusWidget(handler: remotePlayHandler), // pass your handler here
         ],
       ),
-      body: isLoadingModels
+      body: isLoadingModels || isLoadingAddresses
           ? Center(
               child: Container(
                 padding: EdgeInsets.all(20),
                 child: Row(mainAxisAlignment: MainAxisAlignment.center, children: [CircularProgressIndicator(), SizedBox(width: 20), Text("Loading Models...")]),
               ),
+            )
+          : deviceAddresses == null
+          ? QRScanScreen(
+              onPairingComplete: (macAddresses) async {
+                deviceAddresses = macAddresses;
+                await _saveDeviceAddresses();
+                setState(() {});
+              },
             )
           : Column(
               children: [
